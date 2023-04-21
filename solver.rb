c9a23eaaa79fc1cd620b8bef889079eefd653f5f
lucascomamala <lucascomamala@gmail.com>
2023-04-21 09:57:21 -0600

    Coded factorial for n<0

--- solver.rb
+++ solver.rb
@@ -1,6 +1,10 @@
 class Solver
   def factorial(n)
-    return 1 if n <= 0
+    if n < 0
+      raise StandardError.new "Can't calculate factorial of a negative number"
+    end
+
+    return 1 if n <= 1
 
     n * factorial(n-1)
   end
