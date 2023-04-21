811b5155587a49072eb051da54fcc0c39f12c8d3
lucascomamala <lucascomamala@gmail.com>
2023-04-21 09:30:47 -0600

    Created test for factorial(0)

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -2,4 +2,11 @@ require 'rspec'
 require_relative '../solver'
 
 RSpec.describe Solver do
+  describe '#factorial' do
+    context 'when 0 is given' do
+      it 'should return 1' do
+        expect(subject.factorial(0)).to eq(1)
+      end
+    end
+  end
 end
